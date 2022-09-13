0fb84ecad5ee3a814804b4aefc669775b03f9f2d
VNavrockij <vitalik.nav@gmail.com>
2022-09-13 17:00:01 +0300

    feat: minor logic for Equtable

--- Tree.playground/Contents.swift
+++ Tree.playground/Contents.swift
@@ -22,9 +22,47 @@ class TreeNode {
     }
 }
 
-var root = TreeNode(data: "Planting my first seed!")
-print(root.children.isEmpty)
-let branch1 = TreeNode(data: "Your tree is ")
-root.addChild(branch1)
-root.addChild("growing every day.")
-print(root.children.isEmpty)
+// Add extension below:
+extension TreeNode: Equatable {
+    static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return lhs.data == rhs.data && lhs.children == rhs.children
+    }
+    
+    
+}
+
+
+
+
+// E. A. Poe Family Tree
+var patriarch = TreeNode(data: "David Poe")
+var child1 = TreeNode(data: "John Poe")
+var child2 = TreeNode(data: "William Poe")
+var child3 = TreeNode(data: "George Poe")
+var child4 = TreeNode(data: "David Poe")
+var child5 = TreeNode(data: "Samuel Poe")
+var child6 = TreeNode(data: "Maria Poe (Clemm)")
+var child7 = TreeNode(data: "Elizabeth Poe")
+var grandchild1 = TreeNode(data: "William Poe")
+var grandchild2 = TreeNode(data: "Edgar Allan Poe")
+var grandchild3 = TreeNode(data: "Rosalie Poe")
+var grandchild4 = TreeNode(data: "Henry Clemm")
+var grandchild5 = TreeNode(data: "Virginia Marie Clemm")
+var grandchild6 = TreeNode(data: "Virigina Eliza Clemm")
+patriarch.addChild(child1)
+patriarch.addChild(child2)
+patriarch.addChild(child3)
+patriarch.addChild(child4)
+patriarch.addChild(child5)
+patriarch.addChild(child6)
+patriarch.addChild(child7)
+child4.addChild(grandchild1)
+child4.addChild(grandchild2)
+child4.addChild(grandchild3)
+child6.addChild(grandchild4)
+child6.addChild(grandchild5)
+child6.addChild(grandchild6)
+
+// Add Equatable tests here:
+print(patriarch == child4)
+print(child2 == grandchild1)
