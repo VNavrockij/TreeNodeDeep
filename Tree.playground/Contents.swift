79c28fe144f876b0441c7bacaf48d8992b2f1acc
VNavrockij <vitalik.nav@gmail.com>
2022-09-26 16:18:13 +0300

    add: depthFirstTraversal()

--- Tree.playground/Contents.swift
+++ Tree.playground/Contents.swift
@@ -16,7 +16,7 @@ class TreeNode {
     func addChild(_ newChild: TreeNode) {
         children.append(newChild)
     }
-    func addChild(_ newChildData: String) {
+    func addChild(newChildData: String) {
         children.append(TreeNode(data: newChildData))
     }
     func addChildren(_ newChildren: [TreeNode]) {
@@ -47,56 +47,57 @@ extension TreeNode: CustomStringConvertible {
     }
 }
 
-
-// Tree Class
 class Tree {
-    //Instance Variables
     var root: TreeNode
     
-    // Initializer
     init(root: TreeNode) {
         self.root = root
     }
-
     // print Method
     func print() {
         printFrom(root)
     }
-
-    
     // printFrom Method
-    func printFrom(_ currentNode: TreeNode,_ depth: Int = 0) {
-        let depthMarker = String(repeating: "–|", count: depth)
+    func printFrom(_ currentNode: TreeNode, _ depth: Int = 0) {
+        let depthMarker = String(repeating: "--|", count: depth)
         Swift.print("\(depthMarker)\(currentNode)")
         for child in currentNode.children {
-                    printFrom(child, depth + 1)
             printFrom(child, depth + 1)
-                }
+        }
+    }
+
+    // Depth First Traversal
+    func depthFirstTraversal(startingAtNode: TreeNode) {
+        Swift.print("\(startingAtNode) ", terminator: "")
+        for child in startingAtNode.children {
+            depthFirstTraversal(startingAtNode: child)
+        }
     }
 
 
 }
 
-var patriarch = TreeNode(data: "David Poe")
-var child1 = TreeNode(data: "John Poe")
-var child2 = TreeNode(data: "William Poe")
-var child3 = TreeNode(data: "George Poe")
-var child4 = TreeNode(data: "David Poe")
-var child5 = TreeNode(data: "Samuel Poe")
-var child6 = TreeNode(data: "Maria Poe (Clemm)")
-var child7 = TreeNode(data: "Elizabeth Poe")
-var grandchild1 = TreeNode(data: "William Poe")
-var grandchild2 = TreeNode(data: "Edgar Allan Poe")
-var grandchild3 = TreeNode(data: "Rosalie Poe")
-var grandchild4 = TreeNode(data: "Henry Clemm")
-var grandchild5 = TreeNode(data: "Virginia Marie Clemm")
-var grandchild6 = TreeNode(data: "Virigina Eliza Clemm")
-patriarch.addChildren([child1, child2, child3, child4, child5, child6, child7])
-child4.addChildren([grandchild1, grandchild2, grandchild3])
-child6.addChildren([grandchild4, grandchild5, grandchild6])
 
-let poeFamilyTree = Tree(root: patriarch)
+let matriarch = TreeNode(data: "Tracy")
+let tim = TreeNode(data: "Tim")
+let spencer = TreeNode(data: "Spencer")
+let daina = TreeNode(data: "Daina")
+let olivia = TreeNode(data: "Olivia")
+let noah = TreeNode(data: "Noah")
+let zola = TreeNode(data: "Zola the Dog")
+let luna = TreeNode(data: "Luna the Cat")
+let sansa = TreeNode(data: "Sansa the Dog")
+let finnegan = TreeNode(data: "Finnegan the Cat")
+let pepeduke = TreeNode(data: "Pepeduke the Cat")
+let flora = TreeNode(data: "Flora the Puppy")
+matriarch.addChildren([tim, spencer, daina])
+tim.addChildren([olivia, noah, zola, luna])
+spencer.addChild(sansa)
+sansa.addChild(flora)
+daina.addChildren([finnegan, pepeduke])
+let familyTree = Tree(root: matriarch)
 
-poeFamilyTree.printFrom(child4)
-print()
-poeFamilyTree.print()
+ familyTree.depthFirstTraversal(startingAtNode: matriarch)
+
+ print()
+ familyTree.print()
